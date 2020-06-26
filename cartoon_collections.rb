0c78cdc614f1f5b8ee5c4f87a0b634200c158bb5
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-26 02:51:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |element|
+    if element == cheese_types[0]
+
 end
