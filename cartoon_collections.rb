2908ad6f6c45c02ca73b5f8464daad2403dd8c8c
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-26 02:44:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |element| "#{element.capitalize()}!"
+  array.map { |element| "#{element.capitalize()}!"}
   
 end
 
