af154c3313264bb0edc337eed764c7698bada3e2
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-26 02:38:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ def roll_call_dwarves(array)
   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  
 end
 
 def long_planeteer_calls# code an argument here
